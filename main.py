6ad5dc721c19940ff8c5a3a271189cc19896bea1
Telegin Oleg <teleginos@mail.ru>
2024-09-24 19:05:47 +0300

    add validations input

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+
+from typing import Annotated
 
 app = FastAPI()
 
@@ -9,25 +11,58 @@ users = {"1": "Имя: Example, возраст: 18"}
 async def welcome_page() -> str:
     return "Главная страница"
 
+
 @app.get("/users")
 async def get_users() -> dict:
     return users
 
 
 @app.post("/user/{username}/{age}")
-async def post_user(username: str, age: int) -> str:
+async def post_user(
+    username: Annotated[
+        str,
+        Path(
+            min_length=5,
+            max_length=20,
+            description="Enter username",
+            example="UrbanUser",
+        ),
+    ],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")],
+) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, username: str, age: int) -> str:
-    users[user_id] = f"Имя: {username}, возраст: {age}"
+async def update_user(
+    user_id: Annotated[
+        int, Path(ge=1, le=100, description="Enter user_id", example="5")
+    ],
+    username: Annotated[
+        str,
+        Path(
+            min_length=5,
+            max_length=20,
+            description="Enter new username",
+            example="NewUser",
+        ),
+    ],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter new age", example="26")],
+) -> str:
+
+    if str(user_id) not in users:
+        return "User not found"
+
+    users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"User № {user_id} updated"
 
-
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: int) -> str:
+async def delete_user(
+    user_id: Annotated[
+        int, Path(ge=1, le=100, description="Enter user_id", example="5")
+    ]
+) -> str:
     del_users = users.pop(str(user_id))
     return f"Пользователь {del_users} удален из базы"
